faf780cfb455cc25078b78f5dc7cb55b7150ef73
Suleiman <suleiman@56060.in>
2017-05-19 19:22:04 +0530

    added downloadable font

--- app/src/main/java/com/iamsuleiman/androido/FontActivity.java
+++ app/src/main/java/com/iamsuleiman/androido/FontActivity.java
@@ -1,20 +1,64 @@
 package com.iamsuleiman.androido;
 
 import android.graphics.Typeface;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.support.v4.provider.FontRequest;
+import android.support.v4.provider.FontsContractCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class FontActivity extends AppCompatActivity {
+
+    private static final String TAG = FontActivity.class.getSimpleName();
+
+    private Handler mHandler = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_font);
 
-        TextView mTextIntro = (TextView) findViewById(R.id.font_txt_intro);
+        TextView mTextIntro = findViewById(R.id.font_txt_intro);
 
         Typeface typefaceLato = getResources().getFont(R.font.lato_bold);
         mTextIntro.setTypeface(typefaceLato, Typeface.ITALIC);
+
+        FontRequest fontRequest = new FontRequest(
+                "com.google.android.gms.fonts",
+                "com.google.android.gms",
+                "PT_Sans",
+                R.array.com_google_android_gms_fonts_certs);
+
+        FontsContractCompat.FontRequestCallback callback = new FontsContractCompat.FontRequestCallback() {
+            @Override
+            public void onTypefaceRequestFailed(int reason) {
+                Log.e(TAG, "onTypefaceRequestFailed: %d" + reason);
+            }
+
+            @Override
+            public void onTypefaceRetrieved(Typeface typeface) {
+                Log.i(TAG, "onTypefaceRetrieved: ");
+                mTextIntro.setTypeface(typeface);
+            }
+        };
+
+
+        Log.d(TAG, "onCreate: requesting font...");
+        FontsContractCompat.
+                requestFont(this, fontRequest, callback, getHandlerThreadHandler());
+
+    }
+
+    private Handler getHandlerThreadHandler() {
+        if (mHandler == null) {
+            HandlerThread handlerThread = new HandlerThread("fonts");
+            handlerThread.start();
+            mHandler = new Handler(handlerThread.getLooper());
+        }
+        return mHandler;
     }
 }
--- app/src/main/java/com/iamsuleiman/androido/PipActivity.java
+++ app/src/main/java/com/iamsuleiman/androido/PipActivity.java
@@ -1,52 +1,75 @@
 package com.iamsuleiman.androido;
 
+import android.animation.ObjectAnimator;
 import android.app.PictureInPictureArgs;
 import android.content.Intent;
-import android.content.res.Configuration;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.support.annotation.DrawableRes;
+import android.support.transition.TransitionManager;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Surface;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.autofill.AutofillManager;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
-import android.widget.MediaController;
+import android.widget.RelativeLayout;
 import android.widget.VideoView;
 
-public class PipActivity extends AppCompatActivity {
-
+public class PipActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = PipActivity.class.getSimpleName();
 
     private final PictureInPictureArgs mPictureInPictureArgs = new PictureInPictureArgs();
 
     private LinearLayout mLayoutContent;
     private VideoView mVideoView;
+    private LinearLayout mLayoutPlayerControls;
+    private View mPlayerOverlay;
+    private ImageButton mBtnPlay, mBtnRewind, mBtnForward;
+    private Button mBtnPip;
+
+    private RelativeLayout mFramePlayerParent;
+
+    private boolean playerControlsVisible = true;
+
+    private Surface mSurface;
+
+    private Drawable mDrawablePlay;
+    private Drawable mDrawablePause;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pip);
 
+        mDrawablePlay = fetchDrawable(R.drawable.ic_play_circle_filled_black_24dp);
+        mDrawablePause = fetchDrawable(R.drawable.ic_pause_circle_filled_black_24dp);
+
+        mFramePlayerParent = (RelativeLayout) findViewById(R.id.pip_video_layout_parent);
         mLayoutContent = (LinearLayout) findViewById(R.id.pip_content);
         mVideoView = (VideoView) findViewById(R.id.pip_videoview);
-        mVideoView.setMediaController(new MediaController(this));
-        mVideoView.requestFocus();
         String path = "android.resource://" + getPackageName() + "/" + R.raw.sample;
-
         mVideoView.setVideoURI(Uri.parse(path));
-//        mVideoView.start();
 
-        Button button = (Button) findViewById(R.id.pip_btn);
-        button.setOnClickListener(view -> {
-            try {
+        mLayoutPlayerControls = (LinearLayout) findViewById(R.id.pip_video_controls);
+        mPlayerOverlay = findViewById(R.id.pip_video_overlay);
 
-                // maintain aspect ratio while minimizing.
-                float aspectRatio = (float) mVideoView.getWidth() / mVideoView.getHeight();
-                mPictureInPictureArgs.setAspectRatio(aspectRatio);
-                enterPictureInPictureMode(mPictureInPictureArgs);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
+        mBtnPlay = (ImageButton) findViewById(R.id.pip_btn_play);
+        mBtnRewind = (ImageButton) findViewById(R.id.pip_btn_rewind);
+        mBtnForward = (ImageButton) findViewById(R.id.pip_btn_forward);
+        mBtnPip = (Button) findViewById(R.id.pip_btn);
+
+        mFramePlayerParent.setOnClickListener(this);
+        mBtnPlay.setOnClickListener(this);
+        mBtnForward.setOnClickListener(this);
+        mBtnRewind.setOnClickListener(this);
+        mBtnPip.setOnClickListener(this);
 
     }
 
@@ -58,8 +81,67 @@ public class PipActivity extends AppCompatActivity {
         if (!isInPictureInPictureMode) {
             getApplication().startActivity(new Intent(this, getClass())
                     .addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT));
+        } else {
 
         }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.pip_video_layout_parent:
+                playerControlsVisible = !playerControlsVisible;
+                togglePlayerControls(playerControlsVisible);
+                break;
+
+            case R.id.pip_btn:
+                // maintain aspect ratio while minimizing.
+                float aspectRatio = (float) mVideoView.getWidth() / mVideoView.getHeight();
+                mPictureInPictureArgs.setAspectRatio(aspectRatio);
+                mSurface = mVideoView.getHolder().getSurface();
+                enterPictureInPictureMode(mPictureInPictureArgs);
+                break;
+
+            case R.id.pip_btn_rewind:
+                break;
+
+            case R.id.pip_btn_play:
+                toggleVideoPlay();
+                break;
+
+            case R.id.pip_btn_forward:
+                break;
+        }
+    }
+
+    private void toggleVideoPlay() {
+        //play button visible
+        if (mBtnPlay.getDrawable().equals(mDrawablePlay)) {
+            mBtnPlay.setImageDrawable(mDrawablePause);
+            mVideoView.start();
+
+        } else {
+            mBtnPlay.setImageDrawable(mDrawablePlay);
+            if (mVideoView.isPlaying()) mVideoView.pause();
+        }
+    }
+
+
+    private void togglePlayerControls(boolean show) {
+        Log.d(TAG, "togglePlayerControls: show? " + show);
+        // smooth fade out player controls
+        TransitionManager.beginDelayedTransition(mLayoutPlayerControls);
+        ObjectAnimator.ofFloat(mPlayerOverlay, "alpha",
+                mPlayerOverlay.getVisibility() == View.VISIBLE ? 0 : 0.4F)
+                .setDuration(300)
+                .start();
+
+        mLayoutPlayerControls.setVisibility(show ? View.VISIBLE : View.GONE);
+        mPlayerOverlay.setVisibility(show ? View.VISIBLE : View.GONE);
+    }
+
+    private Drawable fetchDrawable(@DrawableRes int id) {
+        return ContextCompat.getDrawable(PipActivity.this, id);
     }
 
 
